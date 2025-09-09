dc20cff6b80ce701ab67a8fff3b07a361a02b157
UltiMaia <plucasmaiadelima@gmail.com>
2025-09-09 18:06:45 -0300

    plotagem do grafico do modelo

--- src/modelo_alternativo.py
+++ src/modelo_alternativo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 dados = np.loadtxt("data/arseniodatasetALT.csv", delimiter=",", skiprows=1)
 
@@ -12,18 +13,25 @@ b = ybar - a*xbar
 print(a)
 print(b)
 
-ytest = a*X + b
+ypred = a*X + b
+
+print(ypred)
 
 #Equação ilustrada:
 print(f"A equação da reta é Y = {a:.2f}x + {b:.2f}")
 
-r_squared = 1 - (np.sum((Y - ytest)**2)
+r_squared = 1 - (np.sum((Y - ypred)**2)
                  /np.sum((Y - ybar)**2))
 
 print(f"R² score: {r_squared:.3f}")
-print("80.4% da variação na variável Arsênio na Água pode ser atribuída ao modelo.")
+print("cerca de 80.4% da variação na variável Arsênio na Água pode ser atribuída ao modelo.")
 
-#fig = px.scatter(x=X, y=Y, 
-#                 trendline="ols", 
-#                 labels={"x":"x","y":"y"})
-#fig.show()
+
+plt.scatter(Y, ypred, color="blue", label="Valores preditos")
+plt.plot(Y, Y, color="red", label="Reta ideal (y = y)")
+
+plt.xlabel("Valor real (Arsenio_Unhas)")
+plt.ylabel("Valor predito")
+plt.title("Regressão Linear - Valores reais vs preditos")
+plt.legend()
+plt.show()
